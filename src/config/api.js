0c499674b10326ebb318415af451f3c145815704
sunyu1 <sunyu1@green-valley.com>
2019-08-29 18:13:38 +0800

    add

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import { baseUrl } from './global'
 import { Promise } from 'q'
 
+// 在http请求层面对其拦截，校验token是否失效
 const get = (url, data) => {
   const wholeUrl = baseUrl + url
   return uni
--- src/pages/homePage/login.vue
+++ src/pages/homePage/login.vue
@@ -1,3 +1,30 @@
 <template>
-  <view>2131</view>
+  <view>
+    <button
+      type="primary"
+      open-type="getUserInfo"
+      @getuserinfo="getUserInfo"
+      withCredentials="true"
+    >
+      微信登录1
+    </button>
+    <button open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">
+      获取电话号码
+    </button>
+  </view>
 </template>
+<script>
+export default {
+  onLoad () {
+
+  },
+  methods: {
+    sss: function () {
+      console.log(1231)
+    },
+    getPhoneNumber: function (e) {
+      console.log('res', e);
+    }
+  }
+}
+</script>
