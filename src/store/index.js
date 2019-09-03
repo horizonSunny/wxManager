468b79be35cd19f9f6dce62fa0319b2427e4ff1a
sunyu1 <sunyu1@green-valley.com>
2019-09-03 10:38:44 +0800

    add

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user from './user'
+import app from './app'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  modules: {},
+  modules: {
+    user,
+    app
+  },
+  getters,
   state: {},
   mutations: {},
   actions: {}
