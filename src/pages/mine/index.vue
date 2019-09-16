1297ec7911e26d7bef396d86c1dda57c89f81e32
sunyu1 <sunyu1@green-valley.com>
2019-09-16 15:33:46 +0800

    add

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -17,7 +17,92 @@
         </view>
       </view>
     </view>
-    <view class="content"> </view>
+    <view class="content">
+      <view class="myOrder">
+        <view>我的订单</view>
+        <view class="lookAll"
+          >查看全部
+          <uni-icon type="" class="iconfont icon-more icon_style"></uni-icon>
+        </view>
+      </view>
+      <view class="orderDivide">
+        <view class="orderItem">
+          <view>
+            <uni-icon
+              type=""
+              class="iconfont icon-tuangou icon_label_style"
+            ></uni-icon>
+          </view>
+          <view>待成团</view>
+        </view>
+        <view class="orderItem">
+          <view>
+            <uni-icon
+              type=""
+              class="iconfont icon-yao icon_label_style"
+            ></uni-icon>
+          </view>
+          <view>待取药</view>
+        </view>
+        <view class="orderItem">
+          <view>
+            <uni-icon
+              type=""
+              class="iconfont icon-yiwancheng icon_label_style"
+            ></uni-icon>
+          </view>
+          <view>已完成</view>
+        </view>
+      </view>
+      <view class="myOperate">
+        <view>
+          <uni-icon
+            type=""
+            class="iconfont icon-youhuiquan icon_label_info"
+          ></uni-icon>
+          <span>我的优惠券</span>
+        </view>
+        <view>
+          <uni-icon type="" class="iconfont icon-more icon_style"></uni-icon>
+        </view>
+      </view>
+      <view class="myOperate">
+        <view>
+          <uni-icon
+            type=""
+            class="iconfont icon-shezhi icon_label_info"
+          ></uni-icon>
+          <span>设置</span>
+        </view>
+        <view>
+          <uni-icon type="" class="iconfont icon-more icon_style"></uni-icon>
+        </view>
+      </view>
+      <view class="myOperate">
+        <view>
+          <uni-icon
+            type=""
+            class="iconfont icon-lianxikefu icon_label_info"
+          ></uni-icon>
+          <span>联系客服</span>
+        </view>
+        <view>
+          <uni-icon type="" class="iconfont icon-more icon_style"></uni-icon>
+        </view>
+      </view>
+      <view class="myOperate">
+        <view>
+          <uni-icon
+            type=""
+            class="iconfont icon-guanyuwomen icon_label_info"
+          ></uni-icon>
+          <span>关于我们</span>
+        </view>
+        <view>
+          <uni-icon type="" class="iconfont icon-more icon_style"></uni-icon>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 <script>
@@ -76,91 +161,62 @@ export default {
   flex: 1;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-  padding: px2rpx(21) px2rpx(15);
-  .buy {
-    width: 100%;
-    height: px2rpx(118);
-    position: relative;
-    img {
-      width: 100%;
-      height: 100%;
+  background: #f3f3f3;
+  .icon_style {
+    font-size: px2rpx(16);
+  }
+  .myOrder {
+    padding: px2rpx(0) px2rpx(10);
+    height: px2rpx(48);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: px2rpx(16);
+    background: #fff;
+    .lookAll {
+      color: #6c6c6c;
     }
-    .text_info {
+    margin-bottom: px2rpx(1);
+  }
+  .orderDivide {
+    padding: px2rpx(0) px2rpx(10);
+    display: flex;
+    background-color: #fff;
+    align-items: center;
+    margin-top: px2rpx(1) px2rpx(0) px2rpx(4);
+    height: px2rpx(90);
+    justify-content: space-between;
+    .orderItem {
+      height: px2rpx(61);
+      width: px2rpx(51);
+      font-size: px2rpx(17);
+      color: #282828;
       display: flex;
       flex-direction: column;
-      justify-content: space-between;
-      position: absolute;
-      right: 15%;
-      top: 50%;
-      height: px2rpx(68);
-      margin-top: px2rpx(-34);
-      .buy_online {
-        font-size: 19px;
-        color: #ffffff;
-        letter-spacing: 0;
-        text-align: justify;
-      }
-      .buy_confirm {
-        display: flex;
-        font-size: 16px;
-        color: #ffda39;
-        letter-spacing: 0;
-        text-align: justify;
-        position: relative;
-        height: px2rpx(22);
-        line-height: px2rpx(22);
-        i {
-          margin-left: px2rpx(5);
-        }
-      }
+      align-items: center;
+    }
+    .icon_label_style {
+      font-size: px2rpx(30);
+      color: #4da08a;
+      margin-top: px2rpx(7);
     }
   }
-  .group_module {
+  .myOperate {
+    padding: px2rpx(0) px2rpx(10);
     display: flex;
-    flex-direction: row;
+    height: px2rpx(58);
+    align-items: center;
     justify-content: space-between;
-    .module {
-      height: px2rpx(130);
-      width: px2rpx(158);
-      position: relative;
-      text-align: center;
-      img {
-        width: 100%;
-        height: 100%;
-      }
-      view {
-        position: absolute;
-        width: px2rpx(72);
-        height: px2rpx(25);
-        font-size: px2rpx(18);
-        top: 70%;
-        left: 50%;
-        margin-left: px2rpx(-36);
-        color: #ffffff;
-      }
+    margin-top: px2rpx(1);
+    background: #fff;
+    font-size: px2rpx(20);
+    color: #282828;
+    .icon_label_info {
+      color: #4da08a;
     }
-  }
-  // arrows 这里是箭头
-  .arrows {
-    background-color: rgba(255, 255, 255, 0);
-  }
-  .arrows:before,
-  .arrows:after {
-    position: absolute;
-    content: "";
-    border-top: px2rpx(10) transparent dashed;
-    border-left: px2rpx(10) transparent dashed;
-    border-bottom: px2rpx(10) transparent dashed;
-    border-right: px2rpx(10) rgba(57, 153, 131, 1) solid;
-  }
-  .arrows:before {
-    border-left: px2rpx(10) #ffda39 solid;
-  }
-  .arrows:after {
-    top: px2rpx(1);
-    /*覆盖并错开1px*/
-    // border-bottom: 10px #fff solid;
+    span {
+      margin-left: px2rpx(20);
+    }
   }
 }
 </style>
