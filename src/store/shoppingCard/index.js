74a0d3105e28b3acfaa164b5a3feff96a02db6ab
sunyu1 <sunyu1@green-valley.com>
2019-09-18 18:19:24 +0800

    add

--- src/store/shoppingCard/index.js
+++ src/store/shoppingCard/index.js
@@ -5,14 +5,18 @@
 import { deepCopy } from '../../utils/index'
 const shoppingCard = {
   state: {
-    commodityInfo: []
+    commodityInfo: [],
+    shoppingCartId: ''
   },
   mutations: {
     /**
      * 因为是数组，所以要使用其它方法做成响应式，
      * 这边的数量改变，列表里面的数量是同一个对象，也会改变
      * */
-
+    RESET_SHOPPING: (state, shopping) => {
+      state.commodityInfo = state.commodityInfo.concat(shopping)
+      state.shoppingCartId = shopping[0]['id']
+    },
     SET_COMMODITY: (state, commodity) => {
       commodity['amount'] += 1
       let contain = state.commodityInfo.some(item => {
@@ -39,11 +43,53 @@ const shoppingCard = {
     }
   },
   actions: {
-    setCommodityInfo({ commit }, commodity) {
-      commit('SET_COMMODITY', commodity)
+    setCommodityInfo({ commit, state }, commodity) {
+      if (state.commodityInfo.length < 1) {
+        this.$http
+          .post('order/shopCart', {
+            createShop: {
+              cartNum: commodity['amount'] + 1,
+              productId: commodity['id']
+            }
+          })
+          .then(res => {
+            state.shoppingCartId = res.data.id
+            commit('SET_COMMODITY', commodity)
+          })
+      } else {
+        this.$http
+          .put('order/shopCart', {
+            createShop: {
+              cartNum: commodity['amount'] + 1,
+              productId: commodity['id'],
+              id: state.shoppingCartId
+            }
+          })
+          .then(res => {
+            state.shoppingCartId = res.data.id
+            commit('SET_COMMODITY', commodity)
+          })
+      }
     },
-    delCommodityInfo({ commit }, commodity) {
-      commit('DEL_COMMODITY', commodity)
+    delCommodityInfo({ commit, state }, commodity) {
+      // commit('DEL_COMMODITY', commodity)
+      this.$http
+        .put('order/shopCart', {
+          createShop: {
+            cartNum: commodity['amount'] - 1,
+            productId: commodity['id'],
+            id: state.shoppingCartId
+          }
+        })
+        .then(res => {
+          state.shoppingCartId = res.data.id
+          commit('DEL_COMMODITY', commodity)
+        })
+    },
+    getShoppingCart({ commit }) {
+      this.$http.get('order/shopCart/info').then(res => {
+        commit('RESET_SHOPPING', res.data)
+      })
     }
   }
 }
